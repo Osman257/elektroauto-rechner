602654eb6b92b2d9bc218e7da8e4a06fb83144e8
Osman257 <59336679+Osman257@users.noreply.github.com>
2025-11-06 14:18:31 +0100

    Update impressum-page.jsx

--- src/pages/impressum-page.jsx
+++ src/pages/impressum-page.jsx
@@ -123,7 +123,7 @@ export default function ImpressumPage() {
             </a>
           </div>
           <div className="text-center text-sm text-gray-500">
-            <p>© 2025 Elektroauto-Rechner. Alle Angaben ohne Gewähr.</p>
+            <p>© 2025 e-Autowelt. Alle Angaben ohne Gewähr.</p>
           </div>
         </div>
       </footer>
